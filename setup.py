97e0199d5f989a3b18aa73180c5fd439b3cc6812
Christian <christianleininger42@gmail.com>
2019-10-29 19:06:19 +0100

    remove files

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='gym_car',
-      version='0.0.1',
-      description='OpenAI Gym environment for parking car',
-      author='Christian Leininger',
-      install_requires=['gym', 'numpy', 'pygame']
-)
