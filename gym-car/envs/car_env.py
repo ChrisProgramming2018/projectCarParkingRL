fix a bug for normalizing the image

gym-car/envs/car_env.py
@@ -23,11 +23,11 @@ class CarEnv(gym.Env):
         self.client.confirmConnection()
         self.client.enableApiControl(True)
         self.car_controls = airsim.CarControls()
-        self.action_space = spaces.Discrete(7)
+        self.action_space = spaces.Discrete(4)
         self.time_step = 0 
-        self.x_pos_goal = 0.6   #0.545647
-        self.y_pos_goal = -2.5  #-1.419126
-        self.z_pos_goal = 0.2   #0.176768
+        self.x_pos_goal = 0.6   #0.545647 0.6
+        self.y_pos_goal = -2.5  #-2.5
+        self.z_pos_goal = 0.2   #0.2
         self.counter_no_state = 0 
         self.w_rot_goal = 1.0    # 0.999967
         self.x_rot_goal = 0.0    # 
@@ -35,27 +35,37 @@ class CarEnv(gym.Env):
         self.z_rot_goal = 0.02    # 0.019440
         self.max_step = 10  # steps to check if blocked 
         self.last_states = queue.deque(maxlen=self.max_step)        
-        self.height = 84
-        self.width = 84  # old 320 
+        self.height = 80
+        self.width = 80  # old 320 
         # self.observation_space = spaces.Box(low=-high, high=high, dtype=np.float32)
-        self.observation_space = spaces.Box(low = 0, high = 255, shape=(self.height, self.width,2))
+        self.observation_space = spaces.Box(low = 0, high = 255, shape=(1, self.height, self.width))
         self.debug_mode = False
         self.goal_counter = 0
         self.count = 0
-
+        self.state = None
+    
     def reset(self):
         """
         This function resets the environment and returns the game state.
         """
         self.client.reset()
         
-        state1 = self._get_state("3")  # forward camera
-        state2 = self._get_state("4") # backward camera
-        state = np.stack((state1, state2), axis=2)
+        self.state = self._get_state("3")  # forward camera
+        # state2 = self._get_state("4") # backward camera
+        #state_n = np.zeros((self.height, self.width))
+        #print("reset", state_n.shape) 
+        #state = np.stack((state_n, state_n))
+        #print("reset 1", state.shape)
+        #state_n = np.expand_dims(state_n, axis=0)
+        #state = np.concatenate((state, state_n))
+        #state = np.concatenate((state, state_n))
+        #print("reset add", state.shape) 
+        #self.state = state
         pose = self.client.simGetVehiclePose()
         reward = self._get_reward(pose)
         self.time_step = 0 
-        return np.array(state)
+        #print("reset end", state.shape) 
+        return np.array(self.state)
 
         
 
@@ -99,6 +109,7 @@ class CarEnv(gym.Env):
         """
         self.time_step += 1 
         self.car_controls.brake = 0
+        #print("action", action)
         if action == 0:
             # go forward
             self.car_controls.throttle = 0.5
@@ -118,13 +129,14 @@ class CarEnv(gym.Env):
             self.client.setCarControls(self.car_controls)
     
         if action == 3:
-            # Go stop
+            #  stop
             self.car_controls.throttle = 0
             self.car_controls.steering = 0
             self.car_controls.brake = 1
             self.client.setCarControls(self.car_controls)
     
         if action == 4:
+
             # Go backward
             self.car_controls.throttle = -0.5
             self.car_controls.steering = 0
@@ -141,36 +153,52 @@ class CarEnv(gym.Env):
             self.car_controls.throttle = -0.5
             self.car_controls.steering = -1
             self.client.setCarControls(self.car_controls)
+        # act  for certain time and stop
+        time.sleep(0.2)
 
+        #print("throttle", self.car_controls.throttle)
+        #print("steering", self.car_controls.steering)
+        self.car_controls.throttle = 0
+        self.car_controls.steering = 0
+        self.car_controls.brake = 1
+        self.client.setCarControls(self.car_controls)
+        
         pose = self.client.simGetVehiclePose()
         reward = self._get_reward(pose)
         done  = False
-        state1 = self._get_state("3")  # forward camera
-        state2 = self._get_state("4") # backward camera
+        self.state = self._get_state("3")  # forward camera
 
-        state = np.stack((state1, state2), axis=2)
+        # state2 = self._get_state("4") # backward camera
+        #state = self.state[1:,:,:]
+        #print("STEP", state.shape)
+        #self.state = np.concatenate((state, np.expand_dims(state1,axis=0)))
+        #print("exit", self.state.shape)
+        #print(" add", self.state.shape)  
         #if reward > -2:
         #    reward = reward + 2 
-        reward /= 10
+        
         if self._is_goal(pose):
             reward = 1
-        else:
-            reward  = max(reward, -3) 
-        return state, reward, done, pose 
+        return self.state, reward, done, pose 
     
+    def set_debug(self, debug=False):
+        """
+        """
+        print("my env")
+        self.debug_mode = debug
+
     def process_image(self, state):
         """ 
         """
-        debug_mode = False
-        if debug_mode:
+        if self.debug_mode:
             img = Image.fromarray(state, 'RGB')
             img.show()
             img.close()
 
         state =  cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
         state = cv2.resize(state,(self.width, self.height))
-        # normailze (values between 0 and 1)
-        state = state / 255
+        # normailze (values between 0 and 1) done in the main program
+        
         return state
 
     
@@ -196,7 +224,19 @@ class CarEnv(gym.Env):
         y_r_dif = math.sqrt((y_rot - self.y_rot_goal)**2)
         z_r_dif = math.sqrt((z_rot - self.z_rot_goal)**2)
         dif = x_dif + y_dif + z_dif + x_r_dif + y_r_dif + z_r_dif
-        return -dif
+        if y_dif >= 3:
+            reward = -1
+        else:
+            reward  = max(-dif, -3)
+        
+        if dif < 2:
+            reward = 0.5**abs(dif)
+        if dif < 0.3:
+            reward = 1
+
+        
+        return reward
+
 
 
 
@@ -218,7 +258,7 @@ class CarEnv(gym.Env):
         x_r_dif = math.sqrt((x_rot - self.x_rot_goal)**2)
         y_r_dif = math.sqrt((y_rot - self.y_rot_goal)**2)
         z_r_dif = math.sqrt((z_rot - self.z_rot_goal)**2)
-        eps = 0.25
+        eps = 0.3
         if self.debug_mode:
             debug_message = '                                position difference of x: {:.2f}, '
             debug_message += 'y: {:.2f}, z: {:.2f}, x_r: '
@@ -232,26 +272,6 @@ class CarEnv(gym.Env):
 
         
 
-    def _is_over(self):
-        """
-        Returns a string and a bool in which way the game is
-        over or not.
-        """
-        if len(self.last_states) < self.max_step:
-            return False
-        # check if positon changed
-        allowed_error = 0.1
-        the_same = 0
-        for i in range(1,self.max_step):
-            if abs(self.last_states[0].position.x_val- self.last_states[i].position.x_val) <= allowed_error:
-                the_same += 1
-            if abs(self.last_states[0].position.y_val- self.last_states[i].position.y_val) <= allowed_error:
-                the_same += 1
-            if abs(self.last_states[0].position.z_val- self.last_states[i].position.z_val) <= allowed_error:
-                the_same += 1
-        if the_same >=25:
-            return True
-        return False
 
     def _get_state(self, camera):
         """ if camera is 3 forward and 4 backward
@@ -268,33 +288,13 @@ class CarEnv(gym.Env):
             if img1d.shape[0] == 268800:
                 state_exists = True
                 state= img1d.reshape(response.height, response.width, 3) # reshape array to 3 channel image
+                #print("state", state)
                 #img = Image.fromarray(state, 'RGB')
                 #text = 'my-{}.png'.format(self.count)
                 #self.count +=1
                 #img.save(text)
+
                 state = self.process_image(state) 
         if state_exists == False:
             state = np.zeros((self.height, self.width))
         return state
-        
-
-    def _is_over(self):
-        """
-        Returns a string and a bool in which way the game is
-        over or not.
-        """
-        if len(self.last_states) < self.max_step:
-            return False
-        # check if positon changed
-        allowed_error = 0.1
-        the_same = 0
-        for i in range(1,self.max_step):
-            if abs(self.last_states[0].position.x_val- self.last_states[i].position.x_val) <= allowed_error:
-                the_same += 1
-            if abs(self.last_states[0].position.y_val- self.last_states[i].position.y_val) <= allowed_error:
-                the_same += 1
-            if abs(self.last_states[0].position.z_val- self.last_states[i].position.z_val) <= allowed_error:
-                the_same += 1
-        if the_same >=25:
-            return True
-        return False